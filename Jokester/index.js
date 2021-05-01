cf7bd064bff451902dceb377f3ef10c847babd80
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-05-02 01:19:29 +0530

    using color package

--- Jokester/index.js
+++ Jokester/index.js
@@ -1,4 +1,7 @@
 const jokes = require('give-me-a-joke')
-jokes.getRandomDadJoke (function(joke) {
-     console.log(joke);
+const colors = require('colors');
+jokes.getRandomDadJoke(function (joke) {
+    console.log(joke.rainbow);
 });
+
+ 
